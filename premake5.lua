36345c3c99718c8932a5ae437a060103a2d255e7
uzlezz-soft <tsushkevichgleb@gmail.com>
2023-11-11 20:52:34 +0300

    stuff

---
+++ premake5.lua
@@ -0,0 +1,25 @@
+function link_spdlog()
+    defines { "SPDLOG_COMPILED_LIB", "SPDLOG_SHARED_LIB" }
+end
+
+project "spdlog"
+    language "C++"
+    cppdialect "C++17"
+
+    defines
+	{
+		"_CRT_SECURE_NO_WARNINGS",
+        "SPDLOG_COMPILED_LIB",
+        "SPDLOG_SHARED_LIB"
+	}
+
+    includedirs
+    {
+        "include"
+    }
+
+    files
+    {
+        "include/**.*",
+        "src/**.*"
+    }
